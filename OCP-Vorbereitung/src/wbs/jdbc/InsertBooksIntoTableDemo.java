8ebb4fef019d270e40202c7a70294ec57a347777
telenair@web.de <telenair@web.de>
2016-08-04 17:44:17 +0200

    neuer Stand 04.08.2016 17:45 Uhr

--- OCP-Vorbereitung/src/wbs/jdbc/InsertBooksIntoTableDemo.java
+++ OCP-Vorbereitung/src/wbs/jdbc/InsertBooksIntoTableDemo.java
@@ -10,41 +10,52 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-/* 
- * wir bestücken unsere buch-tabelle mit einigen büchern.
- * wir verwenden dazu ein PreparedStatement.
- * die bücher lesen wir aus einer geeigneten csv-datei ein.
- */
 public class InsertBooksIntoTableDemo {
 
 	public static void main(String[] args) throws IOException {
 
+		// Verbindung zur Datenbank deklarieren
 		String url = "jdbc:mysql://127.0.0.1:3306/jdbc?useUnicode=yes&characterEncoding=UTF-8";
 		String user = "root";
 		String password = "";
-
+		// Pfad zur insert-sql-Datei deklarieren
 		String path = "resources/sql/insertBooks.sql";
 		String sql = new String(Files.readAllBytes(Paths.get(path)));
-
+		// Pfad zur Eingabedatei deklarieren
 		String pathBooks = "resources/sql/books.txt";
 		File file = new File(pathBooks);
+		// sonstige Deklarationen
 		String token;
 		int count = 0;
+		// Verbindung zur Datenbank aufbauen
 		try (Connection conn = DriverManager.getConnection(url, user, password)) {
+			// PreparedStatement mit insert-sql deklarieren
 			PreparedStatement pstmt = conn.prepareStatement(sql);
 			// einlesen der csv-datei
 			try (Scanner scanner = new Scanner(file)) {
+				// Trennzeichen Semikolon und Zeilenende deklarieren
 				scanner.useDelimiter("[;\n]");
+				// schleife bis alle Elemente verarbeitet sind
 				while (scanner.hasNext()) {
 					token = scanner.next();
 					if (token.length() != 0) {
+						// weil die Zuweisung für das vorcompilierte
+						// PreparedStatement mit dem Wert 1 beginnt
 						count++;
+						// Zuweisen der Werte in das vorcompilierte
+						// PreparedStatement
 						if (count < 4) {
 							pstmt.setString(count, token);
-						}else{
+						} else {
+							// das letzte Feld ist vom typ double
 							pstmt.setDouble(4, Double.parseDouble(token));
-							// ausführen
-							pstmt.execute();
+							// ausführen des PreparedStatements
+							try {
+								pstmt.execute();
+							}catch (SQLException e){
+								System.err.println("FEHLER beim insert in Datenbank: " + e);
+							}
+							// rücksetzen des Feldes count für die nächste Zeile
 							count = 0;
 						}
 					}
@@ -55,6 +66,6 @@ public class InsertBooksIntoTableDemo {
 		} catch (SQLException e) {
 			System.out.println(e);
 		}
+		System.out.println("--- Programm Ende ---");
 	}
-
 }
